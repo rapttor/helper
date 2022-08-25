43f57ef06456f4c94be0c71b92f7f341a009fbc3
Emir Kurtovic <mikscomputers@gmail.com>
2022-08-25 19:19:43 +0200

    E: New functions

--- src/Helper.php
+++ src/Helper.php
@@ -2972,4 +2972,8 @@ class Helper
     public static function base64url_decode($data) { 
         return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT)); 
     } 
+
+
+    
+
 }
